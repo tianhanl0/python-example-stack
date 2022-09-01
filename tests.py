df723e6d24cd7338987d2a5bfaa9f8dcb0280c31
tianhanl0 <112505253+tianhanl0@users.noreply.github.com>
2022-09-01 14:56:17 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -10,10 +10,5 @@ def test_it():
 	Write a simple test.
 	'''
         s = Stack()
-	assert s.size == 0
 	s.push(1)
 	assert s.size() ==1
-	for i in range(1):
-  		s.push(i)
-	assert s.size() = 101
-	raise NotImplementedError('Need to implement a simple test.')
